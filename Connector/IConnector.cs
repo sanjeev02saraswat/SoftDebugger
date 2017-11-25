89869e8b724702aad77dbd2b0d71dd72961b4d38
Sanjeev02Saraswat <sanjeev02saraswat@yandex.com>
2017-11-26 02:50:00 +0530

    ExecuteDataTable Function added in connector for returning DataTable

--- Connector/IConnector.cs
+++ Connector/IConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,7 @@ namespace ConnectorAPI
    public interface IConnector
     {
         bool ExecuteNonQuery(string ConnectionStringName,string StoredProcedureName,Dictionary<string,object> Parameters =null);
+
+        DataTable ExecuteDataTable(string ConnectionStringName, string StoredProcedureName, Dictionary<string, object> Parameters = null);
     }
 }
--- Connector/SqlProcessor/Connector.cs
+++ Connector/SqlProcessor/Connector.cs
@@ -23,6 +23,67 @@ namespace ConnectorAPI
         }
 
         /// <summary>
+        /// Call this function to Execute stored procedure specific Db that will return datatable
+        /// </summary>
+        /// <param name="ConnectionStringName">Pass connection String Name Mention in Configuration</param>
+        /// <param name="StoredProcedureName">Pass stored Procedure Name That you have created</param>
+        /// <param name="Parameters">Pass List of Params if you have else you can skip</param>
+        /// <returns>DataTable Raw if operation will successfull</returns>
+        public DataTable ExecuteDataTable(string ConnectionStringName, string StoredProcedureName, Dictionary<string, object> Parameters = null)
+        {
+            DataTable dt=new DataTable();
+            SqlConnection objSqlConnection = null;
+            try
+            {
+                _logger.addMessage.Add("ExecuteDataTable", "ExecuteNonQuery Method is goint to Execute");
+                SqlContactor objSqlContactor = new SqlContactor();
+                objSqlConnection = objSqlContactor.OpenConnection(ConnectionStringName);
+                _logger.addMessage.Add("ExecuteDataTable", "SqlContactor Open Connection Successfully for " + ConnectionStringName);
+                using (SqlCommand objSqlCommand = new SqlCommand(StoredProcedureName, objSqlConnection))
+                {
+                    objSqlCommand.CommandType = CommandType.StoredProcedure;
+                    if (Parameters != null)
+                    {
+                        _logger.addMessage.Add("ExecuteDataTable", "Parameters is going to Add");
+                        foreach (KeyValuePair<string, object> item in Parameters)
+                        {
+                            if (item.Value != null)
+                            {
+                                _logger.addMessage.Add("ExecuteDataTable", "Parameters Key:" + item.Key + "-Value:" + item.Value.ToString());
+                                objSqlCommand.Parameters.AddWithValue("@" + item.Key, item.Value);
+                            }
+
+                        }
+                    }
+                    SqlDataAdapter da = new SqlDataAdapter(objSqlCommand);
+                    objSqlConnection.Open();
+                    _logger.addMessage.Add("ExecuteDataTable", "Connection open Successfully");
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    _logger.addMessage.Add("ExecuteDataTable", "Record fetached Successfully Successfully");
+                    if (ds.Tables.Count>0)
+                    {
+                        dt = ds.Tables[0];
+                    }else { _logger.addMessage.Add("ExecuteDataTable", "No Record found against this Login"); }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.addMessage.Add("ExecuteNonQuery", "SqlContactor Error during Execution " + ex.ToString());
+                return dt;
+            }
+            finally
+            {
+                SoftLogger.WriteLogImmediateAsync(_logger);
+                objSqlConnection.Close();
+                objSqlConnection.Dispose();
+            }
+            return dt;
+
+        }
+
+        /// <summary>
         /// Call this function to Insert Data into specific Db
         /// </summary>
         /// <param name="ConnectionStringName">Pass connection String Name Mention in Configuration</param>
@@ -46,8 +107,12 @@ namespace ConnectorAPI
                         _logger.addMessage.Add("ExecuteNonQuery", "Parameters is going to Add");
                         foreach (KeyValuePair<string,object> item in Parameters)
                         {
-                            _logger.addMessage.Add("ExecuteNonQuery", "Parameters Key:"+item.Key+"-Value:"+item.Value.ToString());
-                            objSqlCommand.Parameters.AddWithValue("@"+item.Key,item.Value);
+                            if (item.Value!=null)
+                            {
+                                _logger.addMessage.Add("ExecuteNonQuery", "Parameters Key:" + item.Key + "-Value:" + item.Value.ToString());
+                                objSqlCommand.Parameters.AddWithValue("@" + item.Key, item.Value);
+                            }
+                           
                         }
                     }
                     objSqlConnection.Open();
